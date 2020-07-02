af2296240ae54e9ee48bf8f062f044779d6e99cf
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-02 13:53:38 +0300

    Remove unused Ansi escape codes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,19 +10,12 @@ import java.io.IOException;
 public class App {
     //ANSI escape codes to colour my output on the terminal
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
+
     public static final String RED_BOLD_BRIGHT = "\033[1;91m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+
     public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+
     public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) throws IOException {
 
